cc26fd3fa016e06b063b4bb6047f962c018b6ed7
Romeo Balta <7095569+romeobalta@users.noreply.github.com>
2024-03-28 00:51:21 +0000

    improving code rendering and other small stuff

--- src/components/home/section.tsx
+++ src/components/home/section.tsx
@@ -5,7 +5,7 @@ type SectionProps = {
 
 export function Section({ children, title }: SectionProps) {
   return (
-    <div className="w-full mt-16">
+    <div className="section w-full mt-16">
       {title && (
         <h1 className="w-full mb-8 text-lg font-bold text-center font-roboto-condensed uppercase text-muted-foreground">
           {title}
