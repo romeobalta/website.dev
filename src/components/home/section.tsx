7436971d3bad3208624c5ebd22fb4dfb0388b710
Romeo Balta <7095569+romeobalta@users.noreply.github.com>
2024-11-04 09:54:00 +0000

    small margin fix

--- src/components/home/section.tsx
+++ src/components/home/section.tsx
@@ -9,7 +9,7 @@ type SectionProps = {
 
 export function Section({ children, title, className }: SectionProps) {
   return (
-    <div className={cn("section w-full mt-5", className)}>
+    <div className={cn("section w-full mt-5 [&>*:last-child]:mb-0", className)}>
       {title && <H3 className="!mb-5">{title}</H3>}
       {children}
     </div>
